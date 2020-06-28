a50dd69c9e5e01cf349dc206881d875b4c585997
cmite <charles.meuriche@gmail.com>
2020-06-28 18:39:05 +0200

    move data to json

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from enum import Enum
 from random import randrange
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import Circle, Label
-
+import json
 from solar.orbit import Orbit
 from solar.planet import Planet
 from solar.star import Star
@@ -33,8 +33,6 @@ def draw_orbit(fig, stellar_object):
 
 
 if __name__ == "__main__":
-    k_km = 1000.0
-    m_km = 1000000.0
 
     # Define plot
     output_file("solar-system.html")
@@ -44,100 +42,35 @@ if __name__ == "__main__":
     p.sizing_mode = 'scale_width'
     
     # Sun
-    sun = Star(name="Sun", position=(0., 0.), radius=696.342*k_km)
+    sun = Star(name="Sun", position=(0., 0.), radius=696342.0)
     p.circle(x=sun.position[0], y=sun.position[1], radius=sun.radius, fill_color="yellow")
 
     # Planets
-    planets = [
-        Planet(
-            name="Mercury",
-            orbit=Orbit(a=57.9*m_km, e=0.20564, center=sun.position),
-            radius=2.4397*k_km,
-            color='brown'
-        ),
-        Planet(
-            name="Venus",
-            orbit=Orbit(a=108.2*m_km, e=0.00678, center=sun.position),
-            radius=6.0518*k_km,
-            color='pink'
-        ),
-        Planet(
-            name="Earth",
-            orbit=Orbit(a=152.0*m_km, e=0.01671123, center=sun.position),
-            radius=6.378137*k_km,
-            color='blue'
-        ),
-        Planet(
-            name="Mars",
-            orbit=Orbit(a=228.0*m_km, e=0.09339, center=sun.position),
-            radius=3.3962*k_km,
-            color='red'
-        ),
-        Planet(
-            name="Jupiter",
-            orbit=Orbit(a=778.3*m_km, e=0.04939, center=sun.position),
-            radius=71.492*k_km,
-            color='orange'
-        ),
-        Planet(
-            name="Saturn",
-            orbit=Orbit(a=1426.7*m_km, e=0.0539, center=sun.position),
-            radius=60.268*k_km,
-            color='yellow'
-        ),
-        Planet(
-            name="Uranus",
-            orbit=Orbit(a=2870.7*m_km, e=0.04726, center=sun.position),
-            radius=25.559*k_km,
-            color='blue'
-        ),
-        Planet(
-            name="Neptune",
-            orbit=Orbit(a=4498.4*m_km, e=0.00859, center=sun.position),
-            radius=24.764*k_km,
-            color='navy'
-        ),
-        Planet(
-            name="Pluto",
-            orbit=Orbit(a=5900.9*m_km, e=0.25024871, center=sun.position),
-            radius=1.185*k_km,
-            color='palegoldenrod'
+    solar_system = json.load(open("data/solar.json", 'r'))
+    solar_planets = solar_system.get("planets", [])
+    planets = []
+    for planet in solar_planets:
+        planets.append(
+            Planet(
+                name=planet.get("name"),
+                orbit=Orbit(a=planet.get("a"), e=planet.get("e"), center=sun.position),
+                radius=planet.get("radius"),
+                color=planet.get("color")
+            )
         )
-    ]
 
     # Moons
-    moons = [
-        Moon(
-            name="Phobos",
-            orbit=Orbit(a=9.377*k_km, e=0.015, center=planets[SolarPosition.MARS.value].position()),
-            radius=11.267,
-            color='grey'
-        ),
-        Moon(
-            name="Deimos",
-            orbit=Orbit(a=23.460*k_km, e=0.00033, center=planets[SolarPosition.MARS.value].position()),
-            radius=6.2,
-            color='grey'
-        ),
-        Moon(
-            name="Moon",
-            orbit=Orbit(a=384.748*k_km, e=0.0549, center=planets[SolarPosition.EARTH.value].position()),
-            radius=1.7374*k_km,
-            color='grey'
-        ),
-        Moon(
-            name="Triton",
-            orbit=Orbit(a=354.759*k_km, e=0.000016, center=planets[SolarPosition.NEPTUNE.value].position()),
-            radius=1.353*k_km,
-            color='grey'
-        ),
-        Moon(
-            name="Nereid",
-            orbit=Orbit(a=5513.4*k_km, e=0.7417482, center=planets[SolarPosition.NEPTUNE.value].position()),
-            radius=178.5,
-            color='grey'
+    solar_moons = solar_system.get("moons", [])
+    moons = []
+    for moon in solar_moons:
+        moons.append(
+            Moon(
+                name=moon.get("name"),
+                orbit=Orbit(a=moon.get("a"), e=moon.get("e"), center=planets[SolarPosition[moon["planet"]].value].position()),
+                radius=moon.get("radius"),
+                color=moon.get("color")
+            )
         )
-    ]
 
     for planet in planets:
         draw_orbit(p, planet)
